a62f0ced90d8163d3b728371db455fe9b833b278
Myeong Lee <deeper.myeong.lee@gmail.com>
2020-11-18 19:03:08 -0500

    week12

---
+++ week12/regression.R
@@ -0,0 +1,55 @@
+library(stringr)
+library(readr)
+library(dplyr)
+require(utils)
+library(reshape2)
+library(caret)
+library(rpart)
+library(ggplot2)
+library(tidyr)
+library(scales)
+library(lmerTest)
+library("Hmisc")
+library("PerformanceAnalytics")
+
+
+setwd("~/git/AIT722/week12")
+
+# Regressions
+regress <- function (dv, iv, rate){
+  operator<-" ~ "
+  rhs<-iv
+  
+  model_1 <- paste0(
+    dv,
+    operator,
+    rhs
+  ) %>% as.formula
+  
+  fit <- lm(model_1, data=rate)
+  print(summary(fit))
+  print(anova(fit))
+}
+
+rate <- read_delim("data_28cities.csv", delim = ",",col_names = TRUE)
+
+rate$city <- as.factor(rate$city)
+rate <- rate[,-16]
+rate$std_events <- rate$event_total / rate$pop
+
+d <- density(rate$std_events)
+plot(d, "std_events density")
+polygon(d, col="red", border="blue")
+
+# descriptive correlation table
+chart.Correlation(rate[,5:ncol(rate)], histogram=TRUE, pch=19)
+
+# standard number of events as DV
+regress("std_events", "poverty_index", rate)
+
+# what happens if you control for other variables?
+
+# what kinds of community features give rise to people's participation in events?
+
+# how are ethnic_heterogeneity related to poverty level?
+
